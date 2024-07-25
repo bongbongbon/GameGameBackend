c10dfba4329249192bfbc36564463fb77eac0389
bongbong2 <106155992+bongbongbon@users.noreply.github.com>
2024-07-25 22:07:04 +0900

    Update KafkaConfig.java

--- auth-service/src/main/java/com/project/auth_service/config/KafkaConfig.java
+++ auth-service/src/main/java/com/project/auth_service/config/KafkaConfig.java
@@ -28,7 +28,7 @@ public class KafkaConfig {
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "3.37.198.42:29092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(config);
