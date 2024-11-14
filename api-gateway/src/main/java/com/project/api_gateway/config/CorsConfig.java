2c7af16ee17c7a443f0627ecbe857f73b15a7886
rlatnqhd159@naver.com <106155992+bongbongbon@users.noreply.github.com>
2024-11-14 20:04:28 +0900

    delete corsconfig

--- api-gateway/src/main/java/com/project/api_gateway/config/CorsConfig.java
+++ api-gateway/src/main/java/com/project/api_gateway/config/CorsConfig.java
@@ -1,32 +1,32 @@
-package com.project.api_gateway.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-@Configuration
-public class CorsConfig {
-
-    @Bean
-    public CorsWebFilter corsWebFilter() {
-        return new CorsWebFilter(corsConfigurationSource());
-    }
-
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:3000"); // Allow requests from this origin
-        config.addAllowedMethod("*"); // Allow all HTTP methods (GET, POST, etc.)
-        config.addAllowedHeader("*"); // Allow all headers
-        config.setAllowCredentials(true); // Allow credentials (cookies, authorization headers, etc.)
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config); // Apply CORS settings to all routes
-
-        return source;
-    }
-}
+//package com.project.api_gateway.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.reactive.CorsConfigurationSource;
+//import org.springframework.web.cors.reactive.CorsWebFilter;
+//import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+//
+//@Configuration
+//public class CorsConfig {
+//
+//    @Bean
+//    public CorsWebFilter corsWebFilter() {
+//        return new CorsWebFilter(corsConfigurationSource());
+//    }
+//
+//
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.addAllowedOrigin("http://localhost:3000"); // Allow requests from this origin
+//        config.addAllowedMethod("*"); // Allow all HTTP methods (GET, POST, etc.)
+//        config.addAllowedHeader("*"); // Allow all headers
+//        config.setAllowCredentials(true); // Allow credentials (cookies, authorization headers, etc.)
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config); // Apply CORS settings to all routes
+//
+//        return source;
+//    }
+//}
